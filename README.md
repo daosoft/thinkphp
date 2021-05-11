75e78f7577d20405a13ab2471420ea916fce61ab
deno <inbox@email.com>
2021-05-11 17:29:39 +0800

    project init

---
+++ README.md
@@ -0,0 +1,10 @@
+OpenPHP
+===============
+
+> 运行环境要求PHP8.0。
+
+## 安装
+
+~~~
+composer create-project openphp/openphp
+~~~
---
+++ app/controller/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\controller;
+
+/**
+ * Class IndexController
+ * @package app\controller
+ */
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return 'hello';
+    }
+}
